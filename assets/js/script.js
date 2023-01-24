0cdca78b4a811b68b6a94ec16c35304eb3b51164
Nikola Lucic <nikola1983@yahoo.com>
2023-01-24 18:19:13 -0500

    creating container with search properties

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,36 @@ const searchBtnEl = document.getElementById("search-btn");
 const cityInput = document.getElementById("city");
 const stateInput = document.getElementById("state");
 const city = document.getElementById("title");
-const temp = document.getElementById("temperature");
-const wind = document.getElementById("wind");
-const humidity = document.getElementById("humidity");
-const descript = document.getElementById("description");
+const temp = document.getElementById("temperatureToday");
+const wind = document.getElementById("windToday");
+const hum = document.getElementById("humidityToday");
+const descript = document.getElementById("descriptionToday");
+let currentDate = document.querySelector(".date");
+
+let dailyWeather = [];
 const apiKey = "a44b30a2ad958821dc48b2b37277f900";
-// console.log(city);
-// console.log(temp);
+
+displayDate();
+
+document.addEventListener("DOMContentLoaded", () => {
+  searchBtnEl.addEventListener("click", getLatLong);
+  let today = JSON.parse(localStorage.getItem("today"));
+  let week = JSON.parse(localStorage.getItem("forecast"));
+  if (today) {
+    document
+      .querySelectorAll(".todayWrapper>p")
+      .forEach((x) => x.classList.remove("hide"));
+    city.textContent = localStorage.getItem("city");
+    temp.textContent = today.temp;
+    hum.textContent = today.humidity;
+    wind.textContent = today.wind_speed;
+    descript.textContent = today.weather[0].description;
+    if (week) {
+      forecast(week);
+    }
+  }
+});
+
 function getLatLong() {
   const cityName = cityInput.value;
   const stateCode = stateInput.value;
@@ -26,9 +49,8 @@ function getLatLong() {
       return response.json();
     })
     .then(function (data) {
-      // console.log(data[0]);
-      // console.log(city);
       city.textContent = data[0].name;
+      localStorage.setItem("city", data[0].name);
       lat = data[0].lat;
       lon = data[0].lon;
     })
@@ -36,27 +58,11 @@ function getLatLong() {
       getWeather();
     });
 }
-var date = new Date();
-const currentDate = document.querySelector(".date");
-const currentDay = document.querySelector(".day");
-let week = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
 
 function displayDate() {
-  var date = new Date();
-  var day = date.getDate();
-  var month = date.getMonth();
-  var year = date.getFullYear();
-  currentDate.innerHTML = day + "/" + month + "/" + year;
+  let date = dayjs().format("dddd, MMMM D, YYYY");
+  currentDate.textContent = date;
 }
-displayDate();
 
 function getWeather() {
   const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial&exclude=minutely,hourly,alert `;
@@ -65,12 +71,45 @@ function getWeather() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      temperature.textContent = data.current.temp;
-      humidity.textContent = data.current.humidity;
+      dailyWeather = data.daily;
+      document
+        .querySelectorAll(".todayWrapper>p")
+        .forEach((x) => x.classList.remove("hide"));
+      temp.textContent = data.current.temp;
+      hum.textContent = data.current.humidity;
       wind.textContent = data.current.wind_speed;
-      description.textContent = data.current.weather[0].description;
+      descript.textContent = data.current.weather[0].description;
+      localStorage.setItem("today", JSON.stringify(data.current));
+      localStorage.setItem("forecast", JSON.stringify(dailyWeather));
+      forecast(dailyWeather);
     });
 }
 
-searchBtnEl.addEventListener("click", getLatLong);
+function forecast(daily) {
+  for (let i = 0; 1 < daily.length; i++) {
+    let div = document.createElement("div");
+    let h2 = document.createElement("h2");
+    let temperature = document.createElement("p");
+    let humidity = document.createElement("p");
+    let windSpeed = document.createElement("p");
+    let description = document.createElement("p");
+    let section = document.getElementById("forecast");
+    div.classList.add("forecastWrapper");
+    temperature.classList.add("temperature");
+    humidity.classList.add("humidity");
+    windSpeed.classList.add("wind");
+    description.classList.add("description");
+    let today = dayjs();
+    h2.innerText = today.add(i + 1, "d").format("dddd");
+    temperature.textContent = daily[i].temp.day;
+    humidity.textContent = daily[i].humidity;
+    windSpeed.textContent = daily[i].wind_speed;
+    description.textContent = daily[i].weather[0].description;
+    div.appendChild(h2);
+    div.appendChild(temperature);
+    div.appendChild(humidity);
+    div.appendChild(windSpeed);
+    div.appendChild(description);
+    section.appendChild(div);
+  }
+}
