3d6ee2592c8690f60a84b578681a1b4d92b90ae3
NikoCro <118748686+NikoCro@users.noreply.github.com>
2023-01-24 22:30:25 -0500

    creating weather elements for each future day
    im using create.element method to add weather conditions to each day of the 7 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,3 +60,52 @@ function displayDate() {
   let date = dayjs().format("dddd, MMMM D, YYYY");
   currentDate.textContent = date;
 }
+function getWeather() {
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial&exclude=minutely,hourly,alert `;
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      dailyWeather = data.daily;
+      document
+        .querySelectorAll(".todayWrapper>p")
+        .forEach((x) => x.classList.remove("hide"));
+      temp.textContent = data.current.temp;
+      hum.textContent = data.current.humidity;
+      wind.textContent = data.current.wind_speed;
+      descript.textContent = data.current.weather[0].description;
+      localStorage.setItem("today", JSON.stringify(data.current));
+      localStorage.setItem("forecast", JSON.stringify(dailyWeather));
+      forecast(dailyWeather);
+    });
+}
+
+function forecast(daily) {
+  for (let i = 0; i < daily.length; i++) {
+    let div = document.createElement("div");
+    let h2 = document.createElement("h2");
+    let temperature = document.createElement("p");
+    let humidity = document.createElement("p");
+    let windSpeed = document.createElement("p");
+    let description = document.createElement("p");
+    let section = document.getElementById("forecast");
+    div.classList.add("forecastWrapper");
+    temperature.classList.add("temperature");
+    humidity.classList.add("humidity");
+    windSpeed.classList.add("wind");
+    description.classList.add("description");
+    let today = dayjs();
+    h2.innerText = today.add(i + 1, "d").format("dddd");
+    temperature.textContent = daily[i].temp.day;
+    humidity.textContent = daily[i].humidity;
+    windSpeed.textContent = daily[i].wind_speed;
+    description.textContent = daily[i].weather[0].description;
+    div.appendChild(h2);
+    div.appendChild(temperature);
+    div.appendChild(humidity);
+    div.appendChild(windSpeed);
+    div.appendChild(description);
+    section.appendChild(div);
+  }
+}
