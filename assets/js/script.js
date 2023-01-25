cb4bda9cd0b61ca4f6f409249c4a407662930e3e
NikoCro <118748686+NikoCro@users.noreply.github.com>
2023-01-24 22:26:23 -0500

    getting coordinates for certain city
    and getting all the weather data using openweather api and saving results into local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,35 @@ const descript = document.getElementById("descriptionToday");
 let currentDate = document.querySelector(".date");
 let dailyWeather = [];
 const apiKey = "a44b30a2ad958821dc48b2b37277f900";
+
+displayDate();
+
+document.addEventListener("DOMContentLoaded", () => {
+  searchBtnEl.addEventListener("click", getLatLong);
+  let today = JSON.parse(localStorage.getItem("today"));
+  let week = JSON.parse(localStorage.getItem("forecast"));
+  if (today) {
+    document
+      .querySelectorAll(".todayWrapper>p")
+      .forEach((x) => x.classList.remove("hide"));
+    city.textContent = localStorage.getItem("city");
+    temp.textContent = today.temp;
+    hum.textContent = today.humidity;
+    wind.textContent = today.wind_speed;
+    descript.textContent = today.weather[0].description;
+    if (week) {
+      forecast(week);
+    }
+  }
+});
+function getLatLong() {
+  const cityName = cityInput.value;
+  const stateCode = stateInput.value;
+  const url =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    cityName +
+    "," +
+    stateCode +
+    ",USA" +
+    "&limit=3&appid=" +
+    apiKey;
